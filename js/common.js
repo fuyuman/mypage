4ef4aa9b33616b9db187b5eda4f125d6eed03c90
Seonhu Park <seonhupark@mindslab.ai>
2023-07-31 22:56:57 +0900

    edit style.css

--- js/common.js
+++ js/common.js
@@ -1,4 +1,8 @@
 window.addEventListener("load",function(){
     const elLoadWrap = document.querySelector(".loading");
+    const elHeader = document.querySelector("header");
+    const elMain = document.querySelector("main");
     elLoadWrap.classList.add("off");
+    elHeader.classList.add("off");
+    elMain.classList.add("off");
 })
